92b31622fd56c53966907ae55f212994166e384c
D3rkian <derkian.f@escolar.ifrn.edu.br>
2025-07-18 20:22:01 +0000

    configuraçao de banco

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 app = Flask(__name__)
 
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+caminho_db = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.getenv('DB_PATH'))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{caminho_db}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
 @app.route('/')
 def hello_world():
-
     return render_template('index.html')
 
-# Permite rodar o app diretamente
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000, debug=True)
